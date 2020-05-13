083e5ec38c9edee3bb566001ba5c2efab7cbaccc
Yuhang Zhao <2546789017@qq.com>
2020-05-13 21:33:15 +0800

    Minor tweaks.
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- main_windows.cpp
+++ main_windows.cpp
@@ -99,16 +99,21 @@ int main(int argc, char *argv[]) {
     QQmlApplicationEngine engine;
     qmlRegisterType<FramelessQuickHelper>("wangwenx190.Utils", 1, 0,
                                           "FramelessHelper");
-    const QUrl url(QString::fromUtf8("qrc:///qml/main.qml"));
-    QObject::connect(
+    const QUrl mainQmlUrl(QString::fromUtf8("qrc:///qml/main.qml"));
+    const QMetaObject::Connection connection = QObject::connect(
         &engine, &QQmlApplicationEngine::objectCreated, &application,
-        [&url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && (url == objUrl)) {
-                QCoreApplication::exit(-1);
+        [&mainQmlUrl, &connection](QObject *object, const QUrl &url) {
+            if (url != mainQmlUrl) {
+                return;
+            }
+            if (!object) {
+                QGuiApplication::exit(-1);
+            } else {
+                QObject::disconnect(connection);
             }
         },
         Qt::QueuedConnection);
-    engine.load(url);
+    engine.load(mainQmlUrl);
 #endif
 
     return QApplication::exec();
