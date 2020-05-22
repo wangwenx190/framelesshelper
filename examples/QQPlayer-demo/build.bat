a4b50d3136e54911a818fa58f71497861fb2cd9d
Yuhang Zhao <2546789017@qq.com>
2020-05-22 21:55:01 +0800

    Minor tweaks of the example project.
    Improve the build script a bit.
    
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- examples/QQPlayer-demo/build.bat
+++ examples/QQPlayer-demo/build.bat
@@ -9,4 +9,5 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin -GNinja ..
 cmake --build .
 windeployqt --force --no-translations --no-system-d3d-compiler --no-virtualkeyboard --no-compiler-runtime --no-angle --no-opengl-sw --dir "%~dp0build\bin\qml" --libdir "%~dp0build\bin" --plugindir "%~dp0build\bin\plugins" --qmldir "%~dp0resources\qml" "%~dp0build\bin\QQPlayer.exe"
 cd /d "%~dp0"
+start explorer /select,"%~dp0build\bin\QQPlayer.exe"
 exit /b
